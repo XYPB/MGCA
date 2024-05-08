377b4cf28aa20a3d133abfadf4e7e99fdfe41459
XYPB <yuexi.du@yale.edu>
2024-05-08 18:49:59 -0400

    fix

--- mgca/datasets/pretrain_dataset.py
+++ mgca/datasets/pretrain_dataset.py
@@ -650,8 +650,6 @@ class EmbedPretrainingDataset(data.Dataset):
         if series_sents is None:
             series_sents = self.path2sent[path]
 
-        print(series_sents)
-
         if len(series_sents) == 0:
             raise Exception("no sentence for path")
 
